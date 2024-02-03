fd9e28b340ee45a21d3cca7e9619c4a2f6810a88
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2024-02-03 16:59:44 -0600

    add dev cards used to turn

--- SpaceCatan.GameLogic/Turn.cs
+++ SpaceCatan.GameLogic/Turn.cs
@@ -1,6 +1,6 @@
 ﻿namespace SpaceCatan.GameLogic;
 
-public sealed record class Turn(RoadToBuild[] Roads, ColonyToBuild[] Colonies, int DevelopmentCardsPurchased, Dictionary<Resource, int> ChangeInResources);
+public sealed record class Turn(RoadToBuild[] Roads, ColonyToBuild[] Colonies, Dictionary<Resource, int> ResourcesTraded, int DevelopmentCardsBought, int DevelopmentCardsUsed);
 
 public readonly record struct RoadToBuild(int X, int Y, Direction Direction);
 public readonly record struct ColonyToBuild(int X, int Y);
