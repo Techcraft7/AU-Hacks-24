5a4656bb02ea0246065a8399b63f22d578001963
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2024-02-03 14:04:52 -0600

    setup frontent project

--- SpaceCatan/Program.cs
+++ SpaceCatan/Program.cs
@@ -1,6 +1,23 @@
+using Auth0.AspNetCore.Authentication;
+using SpaceCatan.Components;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddAuth0WebAppAuthentication(o =>
+{
+    o.Domain = builder.Configuration["Auth0:Domain"]!;
+    o.ClientId = builder.Configuration["Auth0:ClientID"]!;
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
 
 app.Run();
