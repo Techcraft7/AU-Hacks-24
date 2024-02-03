cad66c7f2b86e0bad25446d59b640461067b832c
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2024-02-03 17:01:34 -0600

    add turn index + current player to gamee

--- SpaceCatan.GameLogic/Game.cs
+++ SpaceCatan.GameLogic/Game.cs
@@ -4,6 +4,8 @@ public sealed class Game
 {
 	public Map Map { get; set; } = new();
 	public Player[] Players { get; set; } = new Player[4];
+	public int CurrentPlayer => (turnIndex % Players.Length) + 1;
+	private int turnIndex = 0;
 
 	public Game()
 	{
@@ -11,13 +13,21 @@ public sealed class Game
 		{
 			Players[i] = new Player()
 			{
-				ID = i
+				ID = i + 1
 			};
 		}
 	}
 
-	public void MakeTurn(Turn turn)
+	public void MakeTurn(int playerID, Turn turn)
 	{
+		if (playerID < 1 ||  playerID > 4)
+		{
+			return;
+		}
+		Player p = Players[playerID - 1];
 
+		// TODO: check 
+
+		turnIndex++;
 	}
 }
