ef75c809cc64541d54a1e9c554b6d2bd6107f8fc
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2024-02-03 16:49:35 -0600

    add turn record

---
+++ SpaceCatan.GameLogic/Turn.cs
@@ -0,0 +1,6 @@
+﻿namespace SpaceCatan.GameLogic;
+
+public sealed record class Turn(RoadToBuild[] Roads, ColonyToBuild[] Colonies, int DevelopmentCardsPurchased, Dictionary<Resource, int> ChangeInResources);
+
+public readonly record struct RoadToBuild(int X, int Y, Direction Direction);
+public readonly record struct ColonyToBuild(int X, int Y);
