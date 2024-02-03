212830e853e5ff01d81d8170b8fe730831700a69
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2024-02-03 16:57:42 -0600

    make game class

---
+++ SpaceCatan.GameLogic/Game.cs
@@ -0,0 +1,23 @@
+﻿namespace SpaceCatan.GameLogic;
+
+public sealed class Game
+{
+	public Map Map { get; set; } = new();
+	public Player[] Players { get; set; } = new Player[4];
+
+	public Game()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			Players[i] = new Player()
+			{
+				ID = i
+			};
+		}
+	}
+
+	public void MakeTurn(Turn turn)
+	{
+
+	}
+}
