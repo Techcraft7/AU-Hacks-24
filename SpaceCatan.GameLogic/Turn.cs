aa55347b10932c83d05baa7db678fd70e304dbd9
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2024-02-04 02:46:34 -0600

    add clicking roads

--- SpaceCatan.GameLogic/Turn.cs
+++ SpaceCatan.GameLogic/Turn.cs
@@ -2,5 +2,15 @@
 
 public sealed record class Turn(RoadToBuild[] Roads, ColonyToBuild[] Colonies, Dictionary<Resource, int> ResourcesTraded, int DevelopmentCardsBought, bool DevelopmentCardUsed);
 
-public readonly record struct RoadToBuild(int X, int Y, Direction Direction);
+public readonly record struct RoadToBuild(int X, int Y, Direction Direction)
+{
+	public RoadToBuild Opposite() => Direction switch
+	{
+		Direction.UP => new(X, Y - 1, Direction.DOWN),
+		Direction.DOWN => new(X, Y + 1, Direction.UP),
+		Direction.LEFT => new(X - 1, Y, Direction.RIGHT),
+		Direction.RIGHT => new(X + 1, Y, Direction.LEFT),
+		_ => this
+	};
+}
 public readonly record struct ColonyToBuild(int X, int Y);
