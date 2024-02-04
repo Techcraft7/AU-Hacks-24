c6ab59a531366e588db40f51b3cd52697feb0b4e
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2024-02-03 20:25:39 -0600

    fix ef core

---
+++ SpaceCatan.Data/Migrations/20240204022222_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpaceCatan.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "TEXT", nullable: false),
+                    CurrentLobby = table.Column<Guid>(type: "TEXT", nullable: true),
+                    Wins = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- SpaceCatan.Data/SpaceCatanContext.cs
+++ SpaceCatan.Data/SpaceCatanContext.cs
@@ -2,7 +2,21 @@
 
 namespace SpaceCatan.Data;
 
-public class SpaceCatanContext(DbContextOptions options) : DbContext(options)
+public class SpaceCatanContext : DbContext
 {
+	public string DbPath { get; set; }
+
+	public SpaceCatanContext()
+	{
+		var folder = Environment.SpecialFolder.LocalApplicationData;
+		var path = Environment.GetFolderPath(folder);
+		DbPath = Path.Join(path, "SpaceCatan.db");
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlite($"Data Source={DbPath}");
+	}
+
 	public DbSet<User> Users { get; set; }
 }
